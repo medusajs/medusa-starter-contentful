66d9aa7e4e1efdea3f110a568b59401905d6374f
Sebastian Rindom <seb@medusa-commerce.com>
2021-08-24 17:10:47 +0200

    fix: add currency code to region migration

--- contentful-migrations/region.js
+++ contentful-migrations/region.js
@@ -1,18 +1,19 @@
 module.exports = function (migration, context) {
   const region = migration
-    .createContentType("region")
-    .name("Region")
-    .displayField("name");
+    .createContentType('region')
+    .name('Region')
+    .displayField('name');
 
-  region.createField("name").name("Name").type("Symbol").required(true);
-  region.createField("countries").name("Options").type("Object");
+  region.createField('name').name('Name').type('Symbol').required(true);
+  region.createField('countries').name('Options').type('Object');
   region
-    .createField("paymentProviders")
-    .name("Payment Providers")
-    .type("Object");
+    .createField('paymentProviders')
+    .name('Payment Providers')
+    .type('Object');
   region
-    .createField("fulfillmentProviders")
-    .name("Fulfillment Providers")
-    .type("Object");
-  region.createField("medusaId").name("Medusa ID").type("Symbol");
+    .createField('fulfillmentProviders')
+    .name('Fulfillment Providers')
+    .type('Object');
+  region.createField('currencyCode').name('Currency Code').type('Symbol');
+  region.createField('medusaId').name('Medusa ID').type('Symbol');
 };
